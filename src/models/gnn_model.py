6a84507a8ef5778cff60969e92e64f29655369e8
ehsan.tavan <ehsan.tavan@partsoftware.com>
2022-10-29 15:18:24 +0330

    [add] add dropout_adj

--- src/models/gnn_model.py
+++ src/models/gnn_model.py
@@ -8,26 +8,30 @@
 
 # ============================ Third Party libs ============================
 import torch
-from torch_geometric.nn import Sequential, GCNConv
+from torch_geometric.utils import dropout_adj
+from torch_geometric.nn import Sequential, GCNConv, GATConv
 
 
 class GraphModel(torch.nn.Module):
     def __init__(self, num_feature, hidden_dim):
         super().__init__()
-        self.conv1 = GCNConv(num_feature, hidden_dim)
-        self.conv2 = GCNConv(hidden_dim, hidden_dim // 2)
-        self.conv3 = GCNConv(hidden_dim // 2, hidden_dim // 4)
+        self.conv1 = GATConv(num_feature, hidden_dim, heads=4)
+        self.conv2 = GATConv(hidden_dim * 4, hidden_dim // 2, heads=4)
+        # self.conv3 = GATConv((hidden_dim // 2)*4, hidden_dim // 4, heads=4)
 
     def forward(self, graph, index):
-        x = self.conv1(graph.x, graph.edge_index)
+        x = graph.x
+        edge_index, edge_attr = dropout_adj(edge_index=graph.edge_index, edge_attr=graph.edge_attr,
+                                            p=0.3, training=self.training)
+        x = self.conv1(x, edge_index, edge_attr)
         x = torch.nn.ReLU()(x)
         x = torch.nn.functional.dropout(x, p=0.2, training=self.training)
-        x = self.conv2(x, graph.edge_index)
+        x = self.conv2(x, edge_index, edge_attr)
         x = torch.nn.ReLU()(x)
-        x = torch.nn.functional.dropout(x, p=0.3, training=self.training)
-        x = self.conv3(x, graph.edge_index)
-        x = torch.nn.ReLU()(x)
-        x = torch.nn.functional.dropout(x, p=0.5, training=self.training)[index]
+        x = torch.nn.functional.dropout(x, p=0.3, training=self.training)[index]
+        # x = self.conv3(x, graph.edge_index, graph.edge_attr)
+        # x = torch.nn.ReLU()(x)
+        # x = torch.nn.functional.dropout(x, p=0.5, training=self.training)[index]
         return x
 
 
@@ -37,11 +41,12 @@ class GCNModel(torch.nn.Module):
         self.lm_model = bert_model
         self.gnn_model = GraphModel(num_feature, hidden_dim)
         self.mean_pooling = torch.nn.AvgPool1d(80)
-        self.classifier = torch.nn.Linear(832, num_classes)
+        self.classifier = torch.nn.Linear(1280, num_classes)
 
     def forward(self, graph, index):
         input_ids, attention_mask = graph.input_ids[index], graph.attention_mask[index]
-        lm_output = self.lm_model(input_ids).last_hidden_state
+        lm_output = self.lm_model(input_ids=input_ids,
+                                  attention_mask=attention_mask).last_hidden_state
         lm_output = self.mean_pooling(lm_output.permute(0, 2, 1)).squeeze(2)
 
         graph.x[index] = lm_output.clone().detach().requires_grad_(True)
@@ -50,53 +55,3 @@ class GCNModel(torch.nn.Module):
         pred = torch.cat((gnn_output, lm_output), 1)
         pred = self.classifier(pred)
         return pred
-
-# class Encoder(torch.nn.Module):
-#     def __init__(self, in_channels, hidden_channels, out_channels):
-#         super().__init__()
-#         self.conv1 = GCNConv(in_channels=in_channels, out_channels=hidden_channels)
-#         self.conv_mu = GCNConv(in_channels=hidden_channels, out_channels=out_channels)
-#         self.conv_logstd = GCNConv(in_channels=hidden_channels, out_channels=out_channels)
-#
-#     def forward(self, x, edge_index):
-#         x = self.conv1(x, edge_index).relu()
-#         return self.conv_mu(x, edge_index), self.conv_logstd(x, edge_index)
-#
-#
-# class Discriminator(torch.nn.Module):
-#     def __init__(self, in_channels, hidden_channels, out_channels):
-#         super().__init__()
-#         self.lin1 = torch.nn.Linear(in_channels, hidden_channels)
-#         self.lin2 = torch.nn.Linear(hidden_channels, hidden_channels)
-#         self.lin3 = torch.nn.Linear(hidden_channels, out_channels)
-#
-#     def forward(self, x):
-#         x = self.lin1(x).relu()
-#         x = self.lin2(x).relu()
-#         return self.lin3(x)
-#
-#
-# class AutoEncoder(LightningModule):
-#     def __init__(self, num_feature: int, hidden_channels: int, out_channels: int):
-#         super().__init__()
-#         self.encoder = Encoder(in_channels=num_feature, hidden_channels=hidden_channels,
-#                                out_channels=out_channels)
-#         self.discriminator = Discriminator(in_channels=hidden_channels,
-#                                            hidden_channels=2 * hidden_channels,
-#                                            out_channels=hidden_channels)
-#         self.model = ARGVA(self.encoder, self.discriminator)
-#
-#         self.save_hyperparameters()
-#
-#     def forward(self, x, edge_index):
-#         return self.model.encode(x, edge_index)
-#
-#     def training_step(self, data, _):
-#         y_hat = self(data.x, data.edge_index)[:data.batch_size]
-#
-#     def configure_optimizers(self):
-#         encoder_optimizer = torch.optim.Adam(self.encoder.parameters(), lr=0.005)
-#         discriminator_optimizer = torch.optim.Adam(self.discriminator.parameters(),
-#                                                    lr=0.001)
-#
-#         return encoder_optimizer, discriminator_optimizer
