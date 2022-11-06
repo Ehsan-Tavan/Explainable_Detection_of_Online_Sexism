2ca9008edb56dec420c54be3735c8c6793a75d84
ehsan.tavan <ehsan.tavan@partsoftware.com>
2022-11-06 17:04:46 +0330

    [add] add InferenceDataset

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -2,4 +2,5 @@
     Explainable Detection of Online Sexism Project:
         Make the importing much shorter
 """
+from .dataset import InferenceDataset
 from .data_module import DataModule
--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -35,3 +35,27 @@ class CustomDataset(torch.utils.data.Dataset):
         return {"inputs_ids": encoded_input["input_ids"].flatten(),
                 "attention_mask": encoded_input["attention_mask"],
                 "labels": torch.tensor(label)}
+
+
+class InferenceDataset(torch.utils.data.Dataset):
+    def __init__(self, texts: List[str], tokenizer, max_len):
+        self.texts = texts
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, item_index):
+        text = self.texts[item_index]
+
+        encoded_input = self.tokenizer.encode_plus(text=text,
+                                                   add_special_tokens=True,
+                                                   max_length=self.max_len,
+                                                   return_tensors="pt",
+                                                   padding="max_length",
+                                                   truncation=True,
+                                                   return_token_type_ids=True)
+
+        return {"inputs_ids": encoded_input["input_ids"].flatten(),
+                "attention_mask": encoded_input["attention_mask"]}
